605ab0ba6e743e3b535678153460f9da06437bf3
darsh-oncloud <dkothari@oncloudconsulting.ca>
2026-06-25 21:17:31 -0400

    Update from NetSuite (Auto)

--- BP_apply_invoice_automation.js
+++ BP_apply_invoice_automation.js
@@ -10,7 +10,7 @@
 define(['N/search', 'N/record', 'N/log'], (search, record, log) => {
 
   // ---------- CONFIG ----------
-  const SAVED_SEARCH_ID     = 'customsearch1782429009740'; // your Collectives Order Deposit search
+  const SAVED_SEARCH_ID     = 'customsearch_collectives_order_deposit_2'; // your Collectives Order Deposit search
   const TARGET_ACCOUNT_ID   = '1032';                 // <-- account to switch the deposit application TO
   const INVOICE_OPEN_STATUS = 'CustInvc:A';                // Open
   // ----------------------------
